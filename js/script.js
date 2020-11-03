74b0264a59603404e622fc7a0f1682487174ae44
FrancescoViterbo <franc.viterbo@gmail.com>
2020-11-03 15:42:35 +0100

    Esercizio base e bonus completi. Funziona anche il keypress

--- js/script.js
+++ js/script.js
@@ -1,27 +1,23 @@
-// Bonus:
-// i pallini si evidenziano in accordo alla img corrispondente
-// Clicchiamo sui pallini e mostriamo l’immagine corrispondente
-// altro esempio: Generiamo i pallini con JS (ma quello che volete liberamente)
-// Consiglio del giorno:
-// Come sempre è importante usare la documentazione di jQuery per scoprire/rinfrescare memoria sulle funzioni che potrebbero esserci utili.
-// Anche perchè qui come gestire eventi da tastiera dovrò vedermelo li.
-
+/* Attivo lo script quando il documento è caricato */
 $( document ).ready(function() {
-    
-    $(document).keydown = function(e) {
-        e.preventDefault();
-        if (e === 39) {
-            prossimaImmagine;
-        } else if (e === 37) {
-            immaginePrecedente;
+    /* Al keypress di destra e sinistra attivo le funzioni per scorrere le immagini */
+    $(document).keydown(function(e) {
+        if ( e.keyCode === 37 ) {
+            e.preventDefault();
+            immaginePrecedente();
+        } else if (e.keyCode === 39) {
+            e.preventDefault();
+            prossimaImmagine();
         }
-    };
+    });
 
+    /* Al click sui pulsanti collego le funzioni relative per scorrere le immagini */
     $("div.prev").click(immaginePrecedente);
     $("div.next").click(prossimaImmagine);
+    $(".nav").children().click(immagineCollegata);
     
     /* Funzioni */
-    
+    /* Sposto la classe "active" all'elemento precendente */
     function immaginePrecedente() {
         var imgAttiva = $(".active");
         imgAttiva.removeClass("active");
@@ -32,7 +28,7 @@ $( document ).ready(function() {
             imgAttiva.prev().addClass("active");
         }
     }
-
+    /* Sposto la classe "active" all'elemento successivo */
     function prossimaImmagine() {
         var imgAttiva = $(".active");
         imgAttiva.removeClass("active");
@@ -42,6 +38,21 @@ $( document ).ready(function() {
         } else {
             imgAttiva.next().addClass("active");
         }
+    }
+    /* Ad ogni pulsante della nav faccio corrispondere un'immagine e attivo
+    la classe active su questa */
+    function immagineCollegata() {
+        /* Rimuovo la classe "active" dal pulsante precedentemente attivato */
+        $(".nav > .active").removeClass("active");
+        /* Aggiungo la classe "active" sul pulsante cliccato */
+        $(this).addClass("active");
+        /* prendo l'index del pulsante cliccato */
+        var indexElemento = $(this).index();
+        /* Rimuovo la classe "active" dall'immagine precedentemente attivata */
+        $("img.active").removeClass("active");
+        /* Con una variabile, collego l'index del pulsante cliccato alle varie immagini */
+        var immagineSpecifica = "img:nth-child(" + (indexElemento + 1) + ")";
+        $(immagineSpecifica).addClass("active");
     }
 
 });
